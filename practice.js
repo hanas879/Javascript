223510c37e59c24a0c612e07ba1a6ac6c476a395
hanas879 <hanas879@milo.life>
2019-02-22 12:13:49 +0100

    if else added

--- practice.js
+++ practice.js
@@ -20,9 +20,54 @@ let question = null
 let num = 0
 
 function randomNumber() {
-	question = prompt("What is your question?")
 	num = Math.floor(Math.random() * 21)
 }
 
-//Testing
-randomNumber()
+
+question = prompt("What is your question?")
+
+if(num === 0){
+
+} else if (num === 1) {
+
+} else if (num === 2) {
+
+} else if (num === 3) {
+
+} else if (num === 4) {
+
+} else if (num === 5) {
+
+} else if (num === 6) {
+
+} else if (num === 7) {
+
+} else if (num === 8) {
+
+} else if (num === 9) {
+
+} else if (num === 10) {
+
+} else if (num === 11) {
+
+} else if (num === 12) {
+
+} else if (num === 13) {
+
+} else if (num === 14) {
+
+} else if (num === 15) {
+
+} else if (num === 16) {
+
+} else if (num === 17) {
+
+} else if (num === 18) {
+
+} else if (num === 19) {
+
+} else if (num === 20) {
+
+}else {
+
+}
